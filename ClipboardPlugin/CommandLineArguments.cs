ccfdb8fd75bd1ccee3835b7aaebea2223533f62e
romesh-abeyawardena <romesh.abeyawardena@bellrockgroup.co.uk>
2023-03-07 15:17:56 +0000

    Initial commit

---
+++ ClipboardPlugin/CommandLineArguments.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ClipboardPlugin;
+
+public record CommandLineArguments
+{
+    public CommandLineArguments(IConfiguration configuration)
+    {
+        configuration.Bind(this);
+    }
+    public string? Text { get; set; }
+}
---
+++ ClipboardPlugin/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClipboardPlugin;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.CommandLine;
+var cb = new ConfigurationBuilder();
+var mappings = new Dictionary<string, string>() { { "-t", "Text" }, { "--text", "Text" } };
+var configuration = cb.Add(new CommandLineConfigurationSource() { Args = args, SwitchMappings = mappings }).Build();
+var commandLineArguments = new CommandLineArguments(configuration);
+
+void WriteColouredText(string message, ConsoleColor consoleColor, params object[] args)
+{
+    var original = Console.ForegroundColor;
+    Console.ForegroundColor = consoleColor;
+    Console.WriteLine(message, args);
+    Console.ForegroundColor = original;
+}
+
+void WriteError(string message, params object[] args)
+{
+    WriteColouredText($"ERROR: {message}", ConsoleColor.Red, args);
+}
+
+void WriteDebug(string message, params object[] args)
+{
+#if DEBUG
+    WriteColouredText($"DEBUG: {message}", ConsoleColor.DarkCyan, args);
+#endif
+}
+
+WriteDebug("{0}", commandLineArguments);
+
+if (!string.IsNullOrWhiteSpace(commandLineArguments.Text))
+{
+    await TextCopy.ClipboardService.SetTextAsync(commandLineArguments.Text);
+}
+else
+    WriteError("Aborted: No text to copy");
+return 0;
