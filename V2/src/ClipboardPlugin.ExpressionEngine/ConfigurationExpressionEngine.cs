5b6c0e68b5a999e9bb1a952158be22f39c47778a
Romesh Abeyawardena <romesh.a@outlook.com>
2025-09-07 19:21:36 +0100

    Add new parameters and project structure for ExpressionEngine
    - Updated `appsettings.json` to include a "Parameters" section with a "now" key set to "getdate" and improved formatting.
    - Added `NCalcAsync` package reference in `ClipboardPlugin.csproj` for expression evaluation.
    - Created `Class1.cs` in the `ClipboardPlugin.ExpressionEngine` namespace with an empty class definition.
    - Established a new project file `ClipboardPlugin.ExpressionEngine.csproj` targeting `net9.0`, with implicit usings and nullable reference types enabled.

---
+++ V2/src/ClipboardPlugin.ExpressionEngine/ConfigurationExpressionEngine.cs
@@ -0,0 +1,15 @@
+﻿using NCalc;
+namespace ClipboardPlugin.ExpressionEngine;
+
+public record ConfigurationExpressionContext : ExpressionContextBase
+{
+
+}
+
+public class ConfigurationExpressionEngine : ExpressionBase<ConfigurationExpressionContext>
+{
+    public NCalc.AsyncExpression Expression(string expression)
+    {
+        return new AsyncExpression(expression, ExpressionOptions.None, System.Globalization.CultureInfo.CurrentCulture);
+    }
+}
