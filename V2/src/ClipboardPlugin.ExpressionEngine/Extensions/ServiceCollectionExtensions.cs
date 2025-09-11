0d4df28badc3727215232513fd42af614ced2486
Romesh Abeyawardena <romesh.a@outlook.com>
2025-09-12 00:00:40 +0100

    Add ServiceCollectionExtensions for DI configuration
    Introduced a new static class `ServiceCollectionExtensions` in the `ClipboardPlugin.ExpressionEngine.Extensions` namespace. Added the method `AddConfigurationExpressionEngine` to register the `IPlaceholderScanner` service as a singleton in the dependency injection container.

---
+++ V2/src/ClipboardPlugin.ExpressionEngine/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using ClipboardPlugin.Abstractions.Expressions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClipboardPlugin.ExpressionEngine.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddConfigurationExpressionEngine(this IServiceCollection services)
+    {
+        return services.AddSingleton<IPlaceholderScanner, PlaceholderScanner>();
+    }
+}
